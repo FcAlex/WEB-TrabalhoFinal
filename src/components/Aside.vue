d8d800b3e9767f7d2f898f3e06d5efc7c2138189
fcalex <contato.fcoalex@gmail.com>
2020-09-29 23:05:25 -0300

    adicao de vue :sparkles:

---
+++ src/components/Aside.vue
@@ -0,0 +1,50 @@
+<template>
+	<aside class="container text-center p-4">
+		<h1><i class="fa fa-paw"></i> Encontre seu novo amigo</h1>
+		<div class="aside row d-flex justify-content-between">
+			<Card
+				imagePath="adocao.png"
+				imageAlt="Um novo lar para um grande parceiro!"
+				titulo="Um novo lar para um grande parceiro!"
+				corpo="Que tal ajudar um animal carente e ainda ganhar um amor incondicional?"
+				rodape="Atualizados 3 minutos atrás"
+			>
+			</Card>
+
+			<Card
+				imagePath="novo_amigo.png"
+				imageAlt="Imagem de capa do card"
+				titulo="Um amigo de verdade!"
+				corpo="Com a adoção, você tira mais um animal da rua, e ajuda traz felicidade e alegria para sua vida. Não seria uma bom negócio?"
+				rodape="Atualizados 3 minutos atrás"
+			>
+			</Card>
+
+			<Card
+				imagePath="animais.png"
+				imageAlt="E então? Vamos lá"
+				titulo="E então? Vamos lá?"
+				corpo="Para realizar esse ato de amor, basta realizar uma rápida pesquisa. Garanto que não vai tomar muito seu tempo!"
+				rodape="Atualizados 3 minutos atrás"
+			>
+			</Card>
+		</div>
+	</aside>
+</template>
+
+<script>
+import Card from "./Card.vue";
+
+export default {
+	name: "Aside",
+	components: {
+		Card,
+	},
+};
+</script>
+
+<style>
+.aside img {
+	width: 10em;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="card-deck p-4 col-4">
+		<div class="card">
+			<img
+				class="card-img-top mx-auto d-block"
+				:src="require(`../assets/card/${imagePath}`)"
+				:alt="imageAlt"
+			/>
+			<div class="card-body">
+				<h5 class="card-title">{{ titulo }}</h5>
+				<p class="card-text">
+					{{ corpo }}
+				</p>
+			</div>
+			<div class="card-footer" v-if="comRodape">
+				<small class="text-muted">
+					{{ rodape }}
+				</small>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Card",
+	props: {
+		imagePath: "",
+		imageAlt: "",
+		titulo: "",
+		corpo: "",
+		rodape: "",
+		comRodape: false,
+		tipoCard: ""
+	},
+};
+</script>
+
+<style>
+</style>
